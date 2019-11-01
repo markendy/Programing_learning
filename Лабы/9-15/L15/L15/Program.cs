04e377c0f972209e510054f4f24942cc2bf05618
markendy <57148996+markendy@users.noreply.github.com>
2019-11-01 10:59:07 +0300

    Friday

---
+++ Лабы/9-15/L15/L15/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory("C:/temp/K1");
+            Directory.CreateDirectory("C:/temp/K2");
+            StreamWriter fl1 = new StreamWriter("C:/temp/K1");
+            StreamWriter fl2 = new StreamWriter("C:/temp/K2");
+            File.Create("C:/temp/K1/t1.txt");
+        }
+    }
+}
