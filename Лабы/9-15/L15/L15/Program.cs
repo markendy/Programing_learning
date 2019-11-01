41215fba85cfe47172c63f418bd3ab49ed7017fb
markendy <57148996+markendy@users.noreply.github.com>
2019-11-01 11:12:40 +0300

    8labs

--- Лабы/9-15/L15/L15/Program.cs
+++ Лабы/9-15/L15/L15/Program.cs
@@ -13,9 +13,22 @@ namespace L15
         {
             Directory.CreateDirectory("C:/temp/K1");
             Directory.CreateDirectory("C:/temp/K2");
-            StreamWriter fl1 = new StreamWriter("C:/temp/K1");
-            StreamWriter fl2 = new StreamWriter("C:/temp/K2");
             File.Create("C:/temp/K1/t1.txt");
+            StreamWriter fl1 = new StreamWriter("C:/temp/K1/t1.txt");
+            StreamWriter fl2 = new StreamWriter("C:/temp/K1/t2.txt");
+            fl1.WriteLine("Иванов Иван Иаванович, 1965 г рождения, место жительства г. Саратов");
+            fl2.WriteLine("Петров Сергей Федорович 1966 г рождения, место жительства г. Энгельс");
+            File.Create("C:/temp/K2/t3.txt");
+            StreamWriter fl3 = new StreamWriter("C:/temp/K2/t3.txt");
+            StreamReader fl1_r = new StreamReader("C:/temp/K1/t1.txt");
+            StreamReader fl2_r = new StreamReader("C:/temp/K1/t2.txt");
+            string fl_s1 = fl1_r.ReadLine();
+            string fl_s2 = fl2_r.ReadLine();
+            fl3.WriteLine(fl_s1);
+            fl3.WriteLine(fl_s2);
+            FileInfo flInf = new FileInfo("C:/temp/K1/t1.txt");
+            flInf = new FileInfo("C:/temp/K1/t2.txt");
+            flInf = new FileInfo("C:/temp/K2/t3.txt");
         }
     }
 }
