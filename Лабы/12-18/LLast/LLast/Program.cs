bf626b86a707140101ca0ad24b1fc3a4691360d0
markendy <57148996+markendy@users.noreply.github.com>
2019-11-09 03:13:12 +0300

    +18labs

--- Лабы/12-18/LLast/LLast/Program.cs
+++ Лабы/12-18/LLast/LLast/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,9 +10,54 @@ namespace LLast
 {
     class Program
     {
+        static string inString;
+        static Regex reg;
+        static MatchCollection mat;
         static void Main(string[] args)
         {
-            string inString;
+            inString = Console.ReadLine();
+            reg = new Regex(@"[а-я|А-Я]");
+            mat = reg.Matches(inString);
+            q1();
+        }
+
+        static bool Find(char ch)
+        {
+            foreach (Match m in mat)
+            {
+                if(ch.ToString() == m.Value)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        static void q1()
+        {       
+            Stack<char> s = new Stack<char>();
+            for (int i = 0; i < inString.Length; i++)
+            {
+                s.Push(inString[i]);
+            }
+            Stack<char> s1 = new Stack<char>();
+            for (int i = 0; i < s.Count;)
+            {
+                if (Find(s.Peek()))
+                {                   
+                    s.Pop();          
+                }
+                else
+                {
+                    s1.Push(s.Pop());
+                }
+            }
+            int t1L = s1.Count;
+            for (int i = 0; i < t1L; i++)
+            {
+                Console.Write(s1.Pop());
+            }
+            Console.WriteLine();
         }
     }
 }
