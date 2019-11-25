e0a9b5aeaeca8364953c6e1fa098b6b1c08d0045
markendy <57148996+markendy@users.noreply.github.com>
2019-11-25 20:24:47 +0300

    last 17 upd

--- Лабы/last 16, 17/17/lab17/lab17/Program.cs
+++ Лабы/last 16, 17/17/lab17/lab17/Program.cs
@@ -1,4 +1,4 @@
-﻿.using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,17 +6,62 @@ using System.Threading.Tasks;
 
 namespace lab17
 {
-    delegate void mydel(ref int[] arr);
+    delegate void mydel(int f, int t);
     class Program
     {
         static void Main(string[] args)
         {
-            int[] a = { 1,6,9,4,6,8,2};
-            mydel del1 = Sort;
-            del1.
+            SortOp(1, 1, mydel
+                );
         }
 
-        static public void Sort(ref int[] arr)
+        static public void SortOp(int mode, int metod, Delegate del_mes)
+        {
+            EnterArr();
+        }
+
+        static public void EnterArr()
+        {
+            Console.WriteLine("Enter lenght array: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[n];
+            Console.WriteLine("Enter element array: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        static public void SortFast(double[] arr, long first, long last)
+        {
+            double p = arr[(last - first) / 2 + first];
+            double temp;
+            long i = first, j = last;
+            while (i <= j)
+            {
+                while (arr[i] < p && i <= last) ++i;
+                while (arr[j] > p && j >= first) --j;
+                if (i <= j)
+                {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                    ++i; --j;
+                }
+            }
+            if (j > first) SortFast(arr, first, j);
+            if (i < last) SortFast(arr, i, last);
+        }
+
+        static public void Viev(int [] arr)
+        {
+            Console.WriteLine("Sort:\n");
+            foreach (var t in arr)
+            {
+                Console.WriteLine(t);
+            }
+        }
+
+        static public void SortPuz(ref int[] arr)
         {
             for(int i =0; i < arr.Length - 1; i++)
             {
@@ -30,7 +75,6 @@ namespace lab17
                     }
                 }
             }
-            Console.WriteLine(del);
         }
     }
 }
